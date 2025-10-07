d59678a3da351f24b3cf878c91b9dc7198914013
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2025-10-07 08:30:31 +0200

    Check if user is following + conditional display

--- src/api/github.ts
+++ src/api/github.ts
@@ -23,3 +23,21 @@ export const searchGithubUser = async (query: string) => {
 
   return data.items;
 };
+
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(`${GITHUB_API_URL}/user/following/${username}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      Accept: 'application/vnd.github+json'
+    }
+  });
+
+  if (res.status === 204) {
+    return true; // following
+  } else if (res.status === 404) {
+    return false; // not following
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || 'Failed to check follow status');
+  }
+};
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,20 +1,43 @@
-import { FaGithubAlt } from 'react-icons/fa';
+import { useQuery } from '@tanstack/react-query';
+import { checkIfFollowingUser } from '../api/github';
+import { FaGithubAlt, FaUserMinus, FaUserPlus } from 'react-icons/fa';
 import type { GithubUser } from '../types';
 
 const UserCard = ({ user }: { user: GithubUser }) => {
+  const { data: isFollowing } = useQuery({
+    queryKey: ['follow-status', user.login],
+    queryFn: () => checkIfFollowingUser(user.login),
+    enabled: !!user.login
+  });
+
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} className="avatar" />
       <h2>{user.name || user.login}</h2>
       <p className="bio">{user.bio}</p>
-      <a
-        href={user.html_url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="profile-btn"
-      >
-        <FaGithubAlt /> View GitHub Profile
-      </a>
+
+      <div className="user-card-buttons">
+        <button className={`follow-btn ${isFollowing ? 'following' : ''}`}>
+          {isFollowing ? (
+            <>
+              <FaUserMinus className="follow-icon" /> Unfollow User
+            </>
+          ) : (
+            <>
+              <FaUserPlus className="follow-icon" /> Follow User
+            </>
+          )}
+        </button>
+
+        <a
+          href={user.html_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="profile-btn"
+        >
+          <FaGithubAlt /> View GitHub Profile
+        </a>
+      </div>
     </div>
   );
 };
