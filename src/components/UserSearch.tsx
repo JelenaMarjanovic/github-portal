c92441a98b0b07d6cc060f2d5a2b15ff373bd955
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2025-10-07 04:15:27 +0200

    Clean up - Export API call for fetching user and implement it within UserSearch component

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { FaGithubAlt } from 'react-icons/fa';
+import { fetchGithubUser } from '../api/github';
 
 const UserSearch = () => {
   const [username, setUsername] = useState('');
@@ -8,20 +9,7 @@ const UserSearch = () => {
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['users', submittedUsername],
-    queryFn: async () => {
-      const res = await fetch(
-        `${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`
-      );
-
-      if (!res.ok) {
-        throw new Error('User not found.');
-      }
-
-      const data = await res.json();
-      console.log(data);
-
-      return data;
-    },
+    queryFn: () => fetchGithubUser(submittedUsername),
     enabled: !!submittedUsername
   });
 
